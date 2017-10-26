14ec3753fa903efbe7c0455281299b905b46df16
Jeffrey Sanford <jeffreysanford@gmail.com>
2017-10-26 13:12:12 -0600

    change all files to gracious girls.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('ng-gratitude-girls App', () => {
+describe('ng-gracious-girls App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Gratitude Girls';
+  title = 'Gracious Girls';
   description = 'Description of the website'
 }
