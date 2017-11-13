43e61f0f255fa3f0a88898d802dd0c8e703bc929
Jeffrey Sanford <jeffreysanford@gmail.com>
2017-11-13 12:32:10 -0700

    added router and landing, about, contact, and services blank template.  Abstracted landing into page templates.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './page-template/header/header.component';
+import { FooterComponent } from './page-template/footer/footer.component';
+import { LandingComponent } from './views/landing/landing.component';
+import { AboutComponent } from './views/about/about.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { ServicesComponent } from './views/services/services.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'landing', component: LandingComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     NgbModule.forRoot(),
     BrowserModule
   ],
